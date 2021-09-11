17363cf66859a37b7f3ee3fceac8e514511d7cee
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-11 01:27:53 -0700

    Update Barcode.py

--- DewmiBot/modules/Barcode.py
+++ DewmiBot/modules/Barcode.py
@@ -25,14 +25,14 @@ async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
         return isinstance(
             (
-                await tbot(functions.channels.GetParticipantRequest(chat, user))
+                await pbot(functions.channels.GetParticipantRequest(chat, user))
             ).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
         )
     if isinstance(chat, types.InputPeerChat):
-        ui = await tbot.get_peer_id(user)
+        ui = await pbot.get_peer_id(user)
         ps = (
-            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
+            await pbot(functions.messages.GetFullChatRequest(chat.chat_id))
         ).full_chat.participants.participants
         return isinstance(
             next((p for p in ps if p.user_id == ui), None),
