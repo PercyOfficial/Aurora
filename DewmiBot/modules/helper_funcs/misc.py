30506179045467e00c7ba5b97c4ff4e9c03e3801
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-11 21:50:24 -0700

    Update misc.py

--- DewmiBot/modules/helper_funcs/misc.py
+++ DewmiBot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("Next", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Close", callback_data="aboutmanu_")]]
+        pairs += [[EqInlineKeyboardButton("close", callback_data="aboutmanu_")]]
 
     return pairs
 
