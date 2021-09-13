c8b57dba243e33e73a6ce5a65f9e40cf249f0bac
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-12 21:05:55 -0700

    Update songs.py

--- DewmiBot/modules/songs.py
+++ DewmiBot/modules/songs.py
@@ -1,88 +1,76 @@
+# Pyrogram Module For Download Song From YouTube 
+# 🍀 © @TeamGroupMenter,@Mr_Dark_Prince
+# ⚠️ Do not edit this lines
+
 import os
-
+import requests
 import aiohttp
+import youtube_dl
+
 from pyrogram import filters
-from pytube import YouTube
-from youtubesearchpython import VideosSearch
-
-from DewmiBot import LOGGER, pbot
-from DewmiBot.utils.ut import get_arg
+from DewmiBot import pbot
+from youtube_search import YoutubeSearch
+from DewmiBot.pyrogramee.errors import capture_err
 
 
-def yt_search(song):
-    videosSearch = VideosSearch(song, limit=1)
-    result = videosSearch.result()
-    if not result:
-        return False
-    else:
-        video_id = result["result"][0]["id"]
-        url = f"https://youtu.be/{video_id}"
-        return url
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-class AioHttp:
-    @staticmethod
-    async def get_json(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.json()
+@pbot.on_message(filters.command(['song']))
+def song(client, message):
 
-    @staticmethod
-    async def get_text(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.text()
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
 
-    @staticmethod
-    async def get_raw(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.read()
-
-
-@pbot.on_message(filters.command("song"))
-async def song(client, message):
-    message.chat.id
-    user_id = message.from_user["id"]
-    args = get_arg(message) + " " + "song"
-    if args.startswith(" "):
-        await message.reply("Enter a song name. Check /help")
-        return ""
-    status = await message.reply("Processing...")
-    video_link = yt_search(args)
-    if not video_link:
-        await status.edit("Song not found.")
-        return ""
-    yt = YouTube(video_link)
-    audio = yt.streams.filter(only_audio=True).first()
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('🔎 Searching your song...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
-        download = audio.download(filename=f"{str(user_id)}")
-    except Exception as ex:
-        await status.edit("Failed to download song")
-        LOGGER.error(ex)
-        return ""
-    os.rename(download, f"{str(user_id)}.mp3")
-    await pbot.send_chat_action(message.chat.id, "upload_audio")
-    await pbot.send_audio(
-        chat_id=message.chat.id,
-        audio=f"{str(user_id)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        reply_to_message_id=message.message_id,
-    )
-    await status.delete()
-    os.remove(f"{str(user_id)}.mp3")
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        #print(results)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
 
 
-__help__ = """
-@szrosebot🇱🇰
- *You can either enter just the song name or both the artist and song
-  name. *
- ❍ /song <songname artist(optional)>*:* uploads the song in it's best quality available
- ❍ /video <songname artist(optional)>*:* uploads the video song in it's best quality available
- ❍ /lyrics <song>*:* returns the lyrics of that song.
- 
-"""
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
 
-__mod_name__ = "Music"
+    except Exception as e:
+        m.edit(
+            "❌ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
+        )
+        print(str(e))
+        return
+    m.edit("`Downloading Song... Please wait ⏱`")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: `YouTube`\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @szrosebot🇱🇰 '
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
+        m.delete()
+    except Exception as e:
+        m.edit('❌ some error')
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
