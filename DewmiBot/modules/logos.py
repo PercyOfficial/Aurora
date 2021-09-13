486d9ba19fd64f63b392c540abd31bdba435f628
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-13 04:27:42 -0700

    Update logos.py

--- DewmiBot/modules/logos.py
+++ DewmiBot/modules/logos.py
@@ -33,7 +33,7 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open(random.choice('./DewmiBot/logo'))
+    img = Image.open(random.choice('DewmiBot/logo'))
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
