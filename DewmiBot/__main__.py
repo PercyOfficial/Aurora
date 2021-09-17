08bb84c60ccd41d30b2c13f16eb47aa7a1934304
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-17 00:58:24 -0700

    Update __main__.py

--- DewmiBot/__main__.py
+++ DewmiBot/__main__.py
@@ -194,7 +194,7 @@ def test(update, context):
 
 @run_async
 def start(update: Update, context: CallbackContext):
-  try:
+    try:
         await message._client.get_chat_member(int("-1001325914694"), message.from_user.id)
     except UserNotParticipant:
         await message.reply_text(
