b1b29497ce089d8334bc3a164c337e2e52006e4b
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-18 18:23:29 -0700

    Update mongo.py

--- DewmiBot/mongo.py
+++ DewmiBot/mongo.py
@@ -5,7 +5,7 @@ from motor import motor_asyncio
 from DewmiBot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from DewmiBot.conf import get_int_key, get_str_key
+from DewmiBot.config import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
