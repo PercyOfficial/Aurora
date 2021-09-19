b6aa6d7470727c0bb3ac6837f27ddc93f038108c
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-19 03:04:17 -0700

    Update devpromoter.py

--- DewmiBot/modules/devpromoter.py
+++ DewmiBot/modules/devpromoter.py
@@ -13,7 +13,7 @@ from DewmiBot.modules.helper_funcs.chat_status import whitelist_plus, dev_plus
 from DewmiBot.modules.helper_funcs.extraction import extract_user
 from DewmiBot.modules.log_channel import gloggable
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), 'cinderella/elevated_users.json')
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), 'DewmiBot/elevated_users.json')
 
 
 def check_user_id(user_id: int, bot: Bot) -> Optional[str]:
