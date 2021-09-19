e78a684ee651e4bb018c46741772161baac3a0f1
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-18 18:33:41 -0700

    Update mongo.py

--- DewmiBot/mongo.py
+++ DewmiBot/mongo.py
@@ -8,7 +8,7 @@ from pymongo.errors import ServerSelectionTimeoutError
 from DewmiBot.config import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27017")
+MONGO_PORT = get_int_key("27018")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "DewmiBot"
 
