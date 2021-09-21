0e472c587973703dda4b1eff70d263a801794386
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-20 18:14:52 -0700

    Update contributors.py

--- DewmiBot/modules/contributors.py
+++ DewmiBot/modules/contributors.py
@@ -1,19 +1,9 @@
 import github  # pyGithub
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
+
 from DewmiBot.services.pyrogram import pbot as client
 
-koyboard = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("Dev", url=f'{co}'),
-                        InlineKeyboardButton("Dev", url=f'{co}'),
-                        InlineKeyboardButton("Dev", url=f'{co}'),
-                    ],
-                    [InlineKeyboardButton("Bot", url="cls")],    
-                ]
-            )       
+  
     
 @client.on_message(filters.command("contributors") & ~filters.edited)
 async def give_cobtribs(c, m):
@@ -24,8 +14,8 @@ async def give_cobtribs(c, m):
     for i in repo.get_contributors():
         n += 1
         co += f"{n}. [{i.login}](https://github.com/{i.login})\n"
-    t = f"**Szrosebot Contributors**\n\n{co}"
-    await m.reply(t, reply_markup=koyboard, disable_web_page_preview=True)
+    t = f"**Szrosebot Contributors**\n\n{co}\n\n\nA Powerful BOT to Make Your Groups Secured and Organized ! ✨"
+    await m.reply(t, disable_web_page_preview=True)
     
 __help__ = """
 @szrosebot🇱🇰
