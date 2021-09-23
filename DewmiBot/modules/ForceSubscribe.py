5b234c4217fce7f534c6f0a2048f6741cfbd2e69
Park Seo Joon <87398748+parkseojoon2005@users.noreply.github.com>
2021-09-23 06:40:11 +0530

    Update ForceSubscribe.py

--- DewmiBot/modules/ForceSubscribe.py
+++ DewmiBot/modules/ForceSubscribe.py
@@ -194,4 +194,4 @@ Note: Only creator of the group can setup me and i will not allow force subscrib
 ⚠️ Note:  /FSub is an alias of /ForceSubscribe
 
 """
-__mod_name__ = "F-Sub"
+__mod_name__ = "F-Sub🎟"
