12206e2ac6908c471cc35282618caaaa25878cb1
Park Seo Joon <87398748+parkseojoon2005@users.noreply.github.com>
2021-09-23 08:11:48 +0530

    Update __repo.py

--- DewmiBot/modules/__repo.py
+++ DewmiBot/modules/__repo.py
@@ -5,18 +5,18 @@ from pyrogram.types import *
 from DewmiBot.config import get_str_key
 from DewmiBot import pbot
 
-REPO_TEXT = "**A Powerful BOT to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 @supunmabot 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Resently\n╰──────────────\n\n»»» @szrosebot «««"
+REPO_TEXT = "**A Powerful and Simple Group Manager Bot To Manage Your Groups Easily ! \n\n↼ Øwñêr ⇀ : 『 PSJ 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Resently\n╰──────────────\n\n»»» @Aurora_x_Bot «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("Repository", url=f"https://github.com/youtubeslgeekshow/sz-rose-bot"),
+        InlineKeyboardButton("Repository", url=f"https://github.com/ParkseoJoon2005/Aurora"),
         InlineKeyboardButton("Video info ", url=f"https://www.youtube.com/channel/UCvYfJcTr8RY72dIapzMqFQA"),
       ],[
-        InlineKeyboardButton("𝑺𝒍 𝑩𝒐𝒕 𝒁𝒐𝒏𝒆 ", url="https://t.me/SL_bot_zone"),
-        InlineKeyboardButton("𝓢𝓛 𝓑𝓸𝓽 𝓒𝓱𝓪𝓽", url="https://t.me/slbotzone"),
+        InlineKeyboardButton("𝑺𝒍 𝑩𝒐𝒕 𝒁𝒐𝒏𝒆 ", url="https://t.me/Hermione_Updates"),
+        InlineKeyboardButton("𝓢𝓛 𝓑𝓸𝓽 𝓒𝓱𝓪𝓽", url="https://t.me/dihan_official"),
       ],[
-        InlineKeyboardButton("rosebot update info", url="https://t.me/szroseupdates"),
-        InlineKeyboardButton("Developer", url="https://t.me/supunmabot"),
+        InlineKeyboardButton("Aurora Bot update info", url="https://t.me/HermioneSupport_Official"),
+        InlineKeyboardButton("Developer", url="https://t.me/Boy_Alone_In_Universe"),
       ]]
     )
   
